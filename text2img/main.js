f00276de2204eda1e245009fc90acba634acc34d
Danil P <slemonide@gmail.com>
2017-10-14 22:53:32 -0700

    More options.

--- text2img/main.js
+++ text2img/main.js
@@ -30,3 +30,43 @@ function main() {
 
     setInterval(render, 1000 / FPS);
 }
+
+function removeSpaces() {
+    var input = document.getElementById('input');
+    input.value = input.value.replace(/ /g, "");
+}
+
+
+function turnTextToSquare() {
+    var input = document.getElementById('input');
+    var inputText = input.value;
+
+    inputText = inputText.replace(/\n/g , "");
+
+    var lineLength = Math.floor(Math.sqrt(inputText.length));
+
+    var output = "";
+
+    for (var i = 0; i < inputText.length; i++) {
+        if ((i + 1) % lineLength === 0) {
+            output += "\n";
+        }
+
+        output += inputText[i]
+    }
+
+    input.value = output
+}
+
+function turnTextToBinary() {
+    var input = document.getElementById('input');
+    var inputText = input.value;
+
+    var output = "";
+
+    for (var i = 0; i < inputText.length; i++) {
+        output += inputText.charCodeAt(i).toString(2)
+    }
+
+    input.value = output
+}
