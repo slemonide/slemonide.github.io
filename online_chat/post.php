921604b3b985664a1a8710d6a4f8fc0e8b7c000a
Danil P <slemonide@gmail.com>
2017-10-20 14:22:34 -0700

    Sanitize the inputs.

--- online_chat/post.php
+++ online_chat/post.php
@@ -1,4 +1,4 @@
 <?php
-$text = $_REQUEST["q"];
+$text = filter_var($_REQUEST["q"], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH, FILTER_FLAG_STRIP_LOW);
 file_put_contents("log.txt", "<li>$text</li>", FILE_APPEND)
 ?>
