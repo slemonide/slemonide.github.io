6b329ae53f041b9ca316453f15bafaa7e636ca52
slemonide <slemonide@gmail.com>
2017-12-09 17:27:44 -0800

    Uglyfy code a bit for compatibility.

--- love/lost/main.lua
+++ love/lost/main.lua
@@ -138,14 +138,65 @@ end
 -- Keypressed
 ------------------------
 
+function getMovement(key)
+    if key == "up" or key == "w" then
+        return {
+            x = player.x,
+            y = player.y - 1
+        }
+    elseif key == "down" or key == "s" then
+        return {
+            x = player.x,
+            y = player.y + 1
+        }
+    elseif key == "left" or key == "a" then
+        return {
+            x = player.x - 1,
+            y = player.y
+        }
+    elseif key == "right" or key == "d" then
+        return {
+            x = player.x + 1,
+            y = player.y
+        }
+    else
+        return false
+    end
+end
+
 function love.keypressed(key)
     if key == "escape" or key == "q" then
         love.event.quit()
     elseif key == "f" then
-        love.window.setFullscreen(not love.window.getFullscreen())
+        love.window.setFullscreen(not (love.window or {}).getFullscreen())
     end
 
-    player.handleKey(key)
+    local movement = getMovement(key)
+
+    if movement and nodes.isWalkable(movement.x, movement.y) and player.blind then
+        player.x = movement.x
+        player.y = movement.y
+
+        if (nodes.isDeadly(movement.x, movement.y)) then
+            player.dead = true
+        end
+
+        if (candles[movement.x] or {})[movement.y] then
+            player.checkpoint.x = movement.x
+            player.checkpoint.y = movement.y
+        end
+
+        if (coins[movement.x] or {})[movement.y] then
+            player.coinsCollected = player.coinsCollected + 1
+            coins[movement.x][movement.y] = false
+        end
+    end
+
+    if key == "space" then
+        player.blind = not player.blind
+    end
+
+
     if (player.dead) then
         draw.deathMessage.x = 0.5 + math.random()
         draw.deathMessage.y = 0.5 + math.random()
